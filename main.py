180957de575bb9714329d41c3399149ad576779d
Thomas Krijnen <t.krijnen@gmail.com>
2023-02-19 20:35:10 +0100

    Reformat main.py

--- main.py
+++ main.py
@@ -21,24 +21,26 @@ class SyntaxError(ValidationError):
 
     def asdict(self, with_message=True):
         return {
-            'type': 'unexpected_token' if isinstance(self.exception, UnexpectedToken) else 'unexpected_character',
-            'lineno': self.exception.line,
-            'column': self.exception.column,
-            'found_type': self.exception.token.type.lower(),
-            'found_value': self.exception.token.value,
-            'expected': sorted(x for x in self.exception.accepts if '__ANON' not in x),
-            'line': self.filecontent.split("\n")[self.exception.line-1],
-            **({'message': str(self)} if with_message else {})
+            "type": "unexpected_token"
+            if isinstance(self.exception, UnexpectedToken)
+            else "unexpected_character",
+            "lineno": self.exception.line,
+            "column": self.exception.column,
+            "found_type": self.exception.token.type.lower(),
+            "found_value": self.exception.token.value,
+            "expected": sorted(x for x in self.exception.accepts if "__ANON" not in x),
+            "line": self.filecontent.split("\n")[self.exception.line - 1],
+            **({"message": str(self)} if with_message else {}),
         }
 
     def __str__(self):
         d = self.asdict(with_message=False)
-        if len(d['expected']) == 1:
-            exp = d['expected'][0]
+        if len(d["expected"]) == 1:
+            exp = d["expected"][0]
         else:
             exp = f"one of {' '.join(d['expected'])}"
 
-        sth = 'character' if d['type'] == 'unexpected_character' else ''
+        sth = "character" if d["type"] == "unexpected_character" else ""
 
         return f"On line {d['lineno']} column {d['column']}:\nUnexpected {sth}{d['found_type']} ('{d['found_value']}')\nExpecting {exp}\n{d['lineno']:05d} | {d['line']}\n        {' ' * (self.exception.column - 1)}^"
 
@@ -51,20 +53,23 @@ class DuplicateNameError(ValidationError):
 
     def asdict(self, with_message=True):
         return {
-            'type': 'duplicate_name',
-            'name': self.name,
-            'lineno': self.linenumbers[0],
-            'line': self.filecontent.split("\n")[self.linenumbers[0]-1],
-            **({'message': str(self)} if with_message else {})
+            "type": "duplicate_name",
+            "name": self.name,
+            "lineno": self.linenumbers[0],
+            "line": self.filecontent.split("\n")[self.linenumbers[0] - 1],
+            **({"message": str(self)} if with_message else {}),
         }
 
     def __str__(self):
         d = self.asdict(with_message=False)
+
         def build():
             yield f"On line {d['lineno']}:\nDuplicate instance name #{d['name']}"
             yield f"{d['lineno']:05d} | {d['line']}"
-            yield ' '* 8 + '^' * len(d['line'].rstrip())
+            yield " " * 8 + "^" * len(d["line"].rstrip())
+
         return "\n".join(build())
+
 
 grammar = r"""
 file: "ISO-10303-21;" header data_section "END-ISO-10303-21;"
@@ -174,6 +179,7 @@ WS: /[ \t\f\r\n]/+
 %ignore COMMENT
 """
 
+
 class Ref:
     def __init__(self, id):
         self.id = id
@@ -237,17 +243,17 @@ class T(Transformer):
 def create_step_entity(entity_tree):
     entity = {}
     t = T(visit_tokens=True).transform(entity_tree)
-    
+
     def get_line_number(t):
         if isinstance(t, Token):
             yield t.line
-    
+
     def traverse(fn, x):
         yield from fn(x)
         if isinstance(x, Tree):
             for c in x.children:
                 yield from traverse(fn, c)
-                
+
     lines = list(traverse(get_line_number, entity_tree))
 
     id_tree = t.children[0].children[0]
@@ -258,7 +264,12 @@ def create_step_entity(entity_tree):
     attributes_tree = t.children[0].children[1].children[1]
     attributes = list(attributes_tree)
 
-    return {"id": entity_id, "type": entity_type, "attributes": attributes, "lines": (min(lines), max(lines))}
+    return {
+        "id": entity_id,
+        "type": entity_type,
+        "attributes": attributes,
+        "lines": (min(lines), max(lines)),
+    }
 
 
 def process_tree(filecontent, file_tree, with_progress):
@@ -276,9 +287,9 @@ def process_tree(filecontent, file_tree, with_progress):
         ent = create_step_entity(entity_tree)
         id_ = int(ent["id"])
         if ents[id_]:
-            raise DuplicateNameError(filecontent, ent['id'], ent['lines'])
+            raise DuplicateNameError(filecontent, ent["id"], ent["lines"])
         ents[id_].append(ent)
-    
+
     return ents
 
 
@@ -296,30 +307,34 @@ def parse(*, filename=None, filecontent=None, with_progress=False, with_tree=Tru
         # of callbacks from the transformer type object, so we can't
         # simply use __getattr__ we need an actual type objects with
         # callback functions for the rules given in the grammar.
-        
+
         # Create a temporary parser just for analysing the grammar
         temp = Lark(grammar, parser="lalr", start="file")
         # Extract the rule names
-        rule_names = filter(lambda s: not s.startswith('_'), set(r.origin.name for r in temp.rules))
+        rule_names = filter(
+            lambda s: not s.startswith("_"), set(r.origin.name for r in temp.rules)
+        )
         null_function = lambda *args: None
         # Create dictionary of methods for type() creation
         methods = {r: null_function for r in rule_names}
-        
+
         # Even in this case we do want to report duplicate identifiers
         # so these need to be captured
-        methods['id'] = lambda *args: args
-        methods['simple_entity_instance'] = lambda tree: instance_identifiers.append((int(tree[0][0][0][1:]), int(tree[0][0][0].line)))
-        
-        NT = type('NullTransformer', (Transformer,), methods)
-        transformer = {'transformer': NT}
-        
+        methods["id"] = lambda *args: args
+        methods["simple_entity_instance"] = lambda tree: instance_identifiers.append(
+            (int(tree[0][0][0][1:]), int(tree[0][0][0].line))
+        )
+
+        NT = type("NullTransformer", (Transformer,), methods)
+        transformer = {"transformer": NT}
+
     parser = Lark(grammar, parser="lalr", start="file", **transformer)
-    
+
     try:
         ast = parser.parse(filecontent)
     except (UnexpectedToken, UnexpectedCharacters) as e:
         raise SyntaxError(filecontent, e)
-   
+
     if with_tree:
         return process_tree(filecontent, ast, with_progress)
     else:
@@ -332,10 +347,11 @@ def parse(*, filename=None, filecontent=None, with_progress=False, with_tree=Tru
                 raise DuplicateNameError(filecontent, iden, [lineno, lineno])
             seen.add(iden)
 
+
 if __name__ == "__main__":
     args = [x for x in sys.argv[1:] if not x.startswith("-")]
     flags = [x for x in sys.argv[1:] if x.startswith("-")]
-    
+
     fn = args[0]
     start_time = time.time()
 
@@ -350,5 +366,6 @@ if __name__ == "__main__":
         else:
             import sys
             import json
+
             json.dump(exc.asdict(), sys.stdout)
         exit(1)
