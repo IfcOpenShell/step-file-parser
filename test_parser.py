8719b3e0ebd2367ca987b7cd35c3b21ffc57bbae
Geert Hesselink <geert.hess@gmail.com>
2025-05-03 02:41:25 +0100

    file.mvd attr available in spf parser

--- test_parser.py
+++ test_parser.py
@@ -64,4 +64,43 @@ def test_parse_valid_header():
 
     for key, val in expected_schema.items():
         assert getattr(f.header.file_schema, key) == val, f"{key} mismatch"
+        
 
+def test_header_only_api():
+    f = open('fixtures/passing_header.ifc', only_header=True)
+    expected_description = {
+        "description": ('ViewDefinition [Alignment-basedView]',),
+        "implementation_level": '2;1',
+    }
+
+    expected_name = {
+        "name": 'Header example2.ifc',
+        "time_stamp": '2022-09-16T10:35:07',
+        "author": ('Evandro Alfieri',),
+        "organization": ('buildingSMART Int.',),
+        "preprocessor_version": 'IFC Motor 1.0',
+        "originating_system": 'Company - Application - 26.0.0.0',
+        "authorization": 'none',
+    }
+
+    expected_schema = {
+        "schema_identifiers": ('IFC4X3_ADD2',),
+    }
+
+    for key, val in expected_description.items():
+        assert getattr(f.header.file_description, key) == val, f"{key} mismatch"
+
+    for key, val in expected_name.items():
+        assert getattr(f.header.file_name, key) == val, f"{key} mismatch"
+
+    for key, val in expected_schema.items():
+        assert getattr(f.header.file_schema, key) == val, f"{key} mismatch"
+
+def test_file_mvd_attr():
+    f = open('fixtures/extended_mvd.ifc', only_header=True)
+    
+    assert 'ReferenceView_V1.2' in f.mvd.view_definitions
+    assert all(i in f.mvd.keywords for i in ['exchange_requirements', 'view_definitions', 'remark', 'comments'])
+    assert 'Ramp' in f.mvd.options['ExcludedObjects']
+    assert f.mvd.Remark['SomeKey'] == 'SomeValue'
+    assert len(f.mvd.comments) == 2
